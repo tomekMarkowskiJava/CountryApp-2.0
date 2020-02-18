123f9ec7780d0ddc92682288fccec2357e61a19e
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-02-18 14:54:57 +0100

    Changed properties for Heroku database

---
+++ src/test/java/com/countryAppSpring/CountryAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countryAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
