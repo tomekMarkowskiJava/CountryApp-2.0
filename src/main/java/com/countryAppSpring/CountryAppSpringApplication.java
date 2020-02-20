09473502a5b02f6b76f280a56d2c1714fb1279a1
tomekMarkowskiJava <tomekmarkowski92@gmail.com>
2020-02-20 11:58:20 +0100

    Test for heroku db

--- src/main/java/com/countryAppSpring/CountryAppSpringApplication.java
+++ src/main/java/com/countryAppSpring/CountryAppSpringApplication.java
@@ -1,12 +1,17 @@
 package com.countryAppSpring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class CountryAppSpringApplication {
 
     public static void main(String[] args) {
